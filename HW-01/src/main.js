5c6ec34a3c7cb4187917f2a1f540181810afc69a
ConorJAT <conorjat75@gmail.com>
2023-09-14 15:06:37 -0400

    Finished hw01 correctly

--- HW-01/src/main.js
+++ HW-01/src/main.js
@@ -1,17 +1,25 @@
 import {getRandom} from "./utils.js";
 
 const init = () => {
-    // Get reference to the button.
+    // Declare babble arrays.
+    let words1;
+    let words2;
+    let words3;
+    
+    // Declare + initialize buttons.
     const btn_1 = document.querySelector("#btn-gen-1");
     const btn_5 = document.querySelector("#btn-gen-5");
 
-    const loadBabble = (count, callback) => {
+    // Declare loadBabble.
+    const loadBabble = () => {
+        // Initialize XHR request.
         const xhr = new XMLHttpRequest();
 
         xhr.onload = e => {
             console.log(`In onLoad - HTTP Status Code - ${e.target.status}`);
 
-            callback(count, e);
+            // Parse and retrieve data from JSON.
+            babbleLoaded(e);
         }
 
         xhr.onerror = e => console.log(`In onError - HTTP Status Code = ${e.target.status}`);
@@ -19,8 +27,8 @@ const init = () => {
         xhr.send();
     }
 
-    // Generate the babble(s).
-    const babbleLoaded = (count, evt) => {
+    // Declare babbleLoaded.
+    const babbleLoaded = evt => {
         let json;
             
         // JSON Guard Code (Try/Catch)
@@ -31,22 +39,36 @@ const init = () => {
             return;
         }
         
-        let w1 = json.words1;
-        let w2 = json.words2;
-        let w3 = json.words3;
+        // Initialize babble arrays.
+        words1 = json.words1;
+        words2 = json.words2;
+        words3 = json.words3;
 
-        // Initialize babble.
-        let html = "";
-        // Concatenate a new babble for every iteration of loop. 
-        for (let i = 0; i < count; i++) html += `${getRandom(w1)} ${getRandom(w2)} ${getRandom(w3)}<br><br>`;
+        // Add onClick event handlers onto buttons.
+        btn_1.onclick = () => { generateBabble(1); };
+        btn_5.onclick = () => { generateBabble(5); };
 
-        // Get reference and change the text of the paragraph element.
-        document.querySelector("#output").innerHTML = html;
+        // Always load webpage with one randomized babble.
+        generateBabble(1);
     }
 
-    // Add 'click' event listener to button -> Generate a babble.
-    btn_1.onclick = () => loadBabble(1, babbleLoaded);
-    btn_5.onclick = () => loadBabble(5, babbleLoaded);
-};
+    // Create babble(s).
+    const generateBabble = count => {
+        // Initialize babble.
+        let babble = "";
+
+        for (let i = 0; i < count; i++)
+        {
+            // Concatenate a new babble for every iteration of loop. 
+            babble += `${getRandom(words1)} ${getRandom(words2)} ${getRandom(words3)}<br><br>`;
+        }
+
+        // Get reference and change the text of the paragraph element.
+        document.querySelector("#output").innerHTML = babble;
+    };
+
+    // Call loadBabble() - Retrieves and parses JSON only once!
+    loadBabble();
+}
 
 init();
