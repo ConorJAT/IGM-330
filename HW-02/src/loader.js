7e2a50786f1e1369c1f75f612352b52023298ab8
Conor Race (RIT Student) <conorjat75@gmail.com>
2023-10-23 12:50:43 -0400

    Created JSON data file and started to implement it

--- HW-02/src/loader.js
+++ HW-02/src/loader.js
@@ -1,8 +1,41 @@
 import * as main from "./main.js";
-window.onload = ()=>{
+window.onload = () => {
 	console.log("window.onload called");
 	// 1 - do preload here - load fonts, images, additional sounds, etc...
-	
+	const loadData = () => {
+		const xhr = new XMLHttpRequest();
+		
+		xhr.onload = e => {
+			avDataLoaded(e);
+		}
+
+		xhr.open("GET", "./data/av-data.json");
+		xhr.send();
+	}
+
+	const avDataLoaded = (e) => {
+		let json;
+
+		try{ json = JSON.parse(e.target.responseText); }
+		catch{ 
+			console.log("JSON parse error!"); 
+			return;
+		}
+
+		document.querySelector("title").innerHTML = json.title;
+
+		let trackList = "";
+		let tracks = json.tracks;
+
+		for(let t of tracks){
+			trackList += `<option value=${t["track-url"]}>${t["track-name"]}</option>`;
+		}
+
+		document.querySelector("#select-track").innerHTML = trackList;
+	}
+
+	loadData();
+
 	// 2 - start up app
 	main.init();
 }
