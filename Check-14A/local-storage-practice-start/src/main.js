73517da742a851c18e53180f864c92b0391f5aeb
Conor Race (RIT Student) <conorjat75@gmail.com>
2023-11-27 12:52:24 -0500

    Started localStorage practice

---
+++ Check-14A/local-storage-practice-start/src/main.js
@@ -0,0 +1,54 @@
+import * as storage from "./storage.js"
+let items = ["???!!!"];
+
+
+// I. declare and implement showItems()
+// - this will show the contents of the items array in the <ol>
+const showItems = () => {
+  // loop though items and stick each array element into an <li>
+  // use array.map()!
+  // update the innerHTML of the <ol> already on the page
+  let html = `${items.map((element) => `<li>${element}</li>`).join("")}`;
+  document.querySelector("ol").innerHTML = html;
+};
+
+// II. declare and implement addItem(str)
+// - this will add `str` to the `items` array (so long as `str` is length greater than 0)
+const addItem = str => {
+  if (str) items.push(str);
+};
+
+
+// Also:
+// - call `addItem()`` when the button is clicked, and also clear out the <input>
+// - and be sure to update .localStorage by calling `writeToLocalStorage("items",items)`
+const setupUI = () => {
+  const addButton = document.querySelector("#btn-add");
+
+  addButton.onclick = () => {
+    let input = document.querySelector("#thing-text");
+    addItem(input.value);
+    input.value = "";
+    showItems();
+
+    storage.writeToLocalStorage("ctr9664", items);
+  };
+};
+  
+
+// When the page loads:
+// - load in the `items` array from storage.js and display the current items
+// you might want to double-check that you loaded an array ...
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
+// ... and if you didn't, set `items` to an empty array
+const init = () => {
+  if (storage.readFromLocalStorage("ctr9664")) { items = storage.readFromLocalStorage("ctr9664"); }
+  else { items = []; }
+
+  showItems();
+  setupUI();
+};
+
+init();
+// Got it working? 
+// - Add a "Clear List" button that empties the items array
